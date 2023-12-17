1f3dfba930f3699705c295af726416ac890f0613
surya <surya.vme005@gmail.com>
2023-12-17 18:08:01 +0530

    project added

---
+++ script.js
@@ -0,0 +1,15 @@
+ScrollReveal({
+    reset:true,
+    
+})
+ScrollReveal().reveal('.about-1,.services-1',{origin:'left',distance:'200px',
+duration:2000,
+delay:200})
+ScrollReveal().reveal('.home-col-2,.services-4',{origin:'right',distance:'200px',
+duration:2000,
+delay:200})
+ScrollReveal().reveal('.services-2',{
+    duration:4000,
+    scale:0.85,
+    
+})
